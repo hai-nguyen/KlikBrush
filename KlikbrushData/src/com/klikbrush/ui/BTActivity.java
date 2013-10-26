61a16fdfc044d6e4d6030bd317a32ee754d96f3e
ultrah <philebert@gmail.com>
2013-10-27 01:09:03 +0200

    BT

---
+++ KlikbrushData/src/com/klikbrush/ui/BTActivity.java
@@ -0,0 +1,169 @@
+package com.klikbrush.ui;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.klikbrush.bluetooth.BluetoothChatService;
+
+public class BTActivity extends FragmentActivity {
+	
+	// Debugging
+	private static final String TAG = "BluetoothChat";
+	private static final boolean D = true;
+
+	// Message types sent from the BluetoothChatService Handler
+	public static final int MESSAGE_STATE_CHANGE = 1;
+	public static final int MESSAGE_READ = 2;
+	public static final int MESSAGE_WRITE = 3;
+	public static final int MESSAGE_DEVICE_NAME = 4;
+	public static final int MESSAGE_TOAST = 5;
+
+	// Key names received from the BluetoothChatService Handler
+	public static final String DEVICE_NAME = "device_name";
+	public static final String TOAST = "toast";
+
+	// Intent request codes
+	private static final int REQUEST_CONNECT_DEVICE = 1;
+	private static final int REQUEST_ENABLE_BT = 2;
+
+	// Layout Views
+	// private TextView mTitle;
+	private Button mSendButton;
+
+	// Name of the connected device
+	private String mConnectedDeviceName = null;
+	// Local Bluetooth adapter
+	private BluetoothAdapter mBluetoothAdapter = null;
+	// Member object for the chat services
+	private BluetoothChatService mChatService = null;
+	private SensorManager mSensorManager;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			Toast.makeText(this, "Bluetooth is not available",
+					Toast.LENGTH_LONG).show();
+			finish();
+			return;
+		}
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		if (!mBluetoothAdapter.isEnabled()) {
+			Intent enableIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+		} else {
+			if (mChatService == null)
+				setupChat();
+		}
+	}
+
+	@Override
+	public synchronized void onResume() {
+		super.onResume();
+		if (D)
+			Log.e(TAG, "+ ON RESUME +");
+
+		if (mChatService != null) {
+			if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+				mChatService.start();
+			}
+		}
+	}
+
+	private void setupChat() {
+		mChatService = new BluetoothChatService(this, mHandler);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		// Stop the Bluetooth chat services
+		if (mChatService != null)
+			mChatService.stop();
+	}
+
+	// The Handler that gets information back from the BluetoothChatService
+	private final Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case MESSAGE_STATE_CHANGE:
+				if (D)
+					Log.i(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
+				switch (msg.arg1) {
+				case BluetoothChatService.STATE_CONNECTED:
+					System.out
+							.println("BTActivity - Device connected");
+					break;
+				case BluetoothChatService.STATE_CONNECTING:
+					break;
+				case BluetoothChatService.STATE_LISTEN:
+				case BluetoothChatService.STATE_NONE:
+					break;
+				}
+				break;
+			case MESSAGE_WRITE:
+				break;
+			case MESSAGE_READ:
+				// a message has arrived!
+				byte[] readBuf = (byte[]) msg.obj;
+				// construct a string from the valid bytes in the buffer
+				String readMessage = new String(readBuf, 0, msg.arg1);
+				processMessage(readMessage);
+				break;
+			case MESSAGE_DEVICE_NAME:
+				// save the connected device's name
+				mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+				Toast.makeText(getApplicationContext(),
+						"Connected to " + mConnectedDeviceName,
+						Toast.LENGTH_SHORT).show();
+				break;
+			case MESSAGE_TOAST:
+				Toast.makeText(getApplicationContext(),
+						msg.getData().getString(TOAST), Toast.LENGTH_SHORT)
+						.show();
+				break;
+			}
+		}
+
+		
+	};
+	
+	protected void processMessage(String readMessage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case REQUEST_ENABLE_BT:
+			// When the request to enable Bluetooth returns
+			if (resultCode == Activity.RESULT_OK) {
+				// Bluetooth is now enabled, so set up a chat session
+				setupChat();
+			} else {
+				// User did not enable Bluetooth or an error occured
+				Log.d(TAG, "BT not enabled");
+				finish();
+			}
+		}
+	}
+
+}
--- KlikbrushData/src/com/klikbrush/ui/BrushingActivity.java
+++ KlikbrushData/src/com/klikbrush/ui/BrushingActivity.java
@@ -1,20 +1,23 @@
 package com.klikbrush.ui;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 import com.example.klikbrushdata.R;
 
-public class BrushingActivity extends FragmentActivity {
+public class BrushingActivity extends BTActivity {
 	
 	TextView txtCounter, txtFrequency, txtStroke;
 	ProgressBar prgPhase;
     int passedSenconds = 0;
 
 
+    @Override
+    protected void processMessage(String message){
+    	Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
