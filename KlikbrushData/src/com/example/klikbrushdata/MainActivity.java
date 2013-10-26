fab31b5236aeb880441e2ce921337dc619d85ddf
ultrah <philebert@gmail.com>
2013-10-26 15:32:48 +0200

    data app ui

--- KlikbrushData/src/com/example/klikbrushdata/MainActivity.java
+++ KlikbrushData/src/com/example/klikbrushdata/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.klikbrushdata;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,26 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        findViewById(R.id.btn_start).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
+        findViewById(R.id.btn_stop).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
     }
 
 
