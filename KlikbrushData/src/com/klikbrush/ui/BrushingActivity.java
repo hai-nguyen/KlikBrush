9bd1d15d0ed1ae1a3bf03c0a04fe4482e95537b1
saqib@razaq.de <saqib@razaq.de>
2013-10-26 18:45:37 +0200

    Implemented function to initialize the UI

--- KlikbrushData/src/com/klikbrush/ui/BrushingActivity.java
+++ KlikbrushData/src/com/klikbrush/ui/BrushingActivity.java
@@ -2,13 +2,25 @@ package com.klikbrush.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
 
 import com.example.klikbrushdata.R;
 
 public class BrushingActivity extends FragmentActivity {
+	
+	TextView txtCounter, txtFrequency, txtStroke;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_brushing);
 	}
+	
+	private void InitUI(){
+		txtCounter = (TextView) findViewById(R.id.txt_counter);
+		txtFrequency = (TextView) findViewById(R.id.txt_frequency);
+		txtStroke = (TextView) findViewById(R.id.txt_strokelength);
+	}
+	
+	
 }
