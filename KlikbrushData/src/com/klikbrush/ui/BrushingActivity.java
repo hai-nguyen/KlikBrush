bb44f399559de1da9297216e228481c8f664616d
saqib@razaq.de <saqib@razaq.de>
2013-10-27 07:36:20 +0100

    share with box

--- KlikbrushData/src/com/klikbrush/ui/BrushingActivity.java
+++ KlikbrushData/src/com/klikbrush/ui/BrushingActivity.java
@@ -1,8 +1,12 @@
 package com.klikbrush.ui;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
@@ -125,5 +129,28 @@ public class BrushingActivity extends BTActivity {
 		}
 
 	}
+	
+	@Override
+	  public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main, menu);
+	    return true;
+	  } 
+	
+	@Override
+	  public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    case R.id.action_box:
+	      Intent launchActivity = new Intent(this, ResultActivity.class);
+	      startActivity(launchActivity);
+	      break;
+	
+
+	    default:
+	      break;
+	    }
+
+	    return true;
+	  } 
 
 }
---
+++ KlikbrushData/src/com/klikbrush/ui/ResultActivity.java
@@ -0,0 +1,16 @@
+package com.klikbrush.ui;
+
+import com.example.klikbrushdata.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_result);
+	}
+
+}
